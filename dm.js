4ff9aef25780ee8474226ec39be36065daceb9a0
hexique <69116911+hexique@users.noreply.github.com>
2025-11-21 07:54:34 +0300

    1.3: final release
    added dm save
    added about.html
    fixed bugs

--- dm.js
+++ dm.js
@@ -1,35 +1,85 @@
 let i = 0
+let history = []
+if(localStorage.getItem("is-first")){
+    displayMessage()
+    i++
+}
+load()
 
-function send(){
+function save(){
+    localStorage.setItem("messages-history", JSON.stringify(history))
+    console.log("saved")
+}
+
+function load(){
+    history = JSON.parse(localStorage.getItem("messages-history"))
+
+    for(let i = 0; i < history.length; i++){
+        if(history[i]["author"] == "you"){
+            displayYourMessage(history[i]["message"], false)
+        } else {
+            displayMessage(false)
+        }
+    }
+}
+
+function displayMessage(saves){
+    if(localStorage.getItem("messages").split("s-p-l-i-t-t-e-r")[i] == "") return
+    document.getElementById("messages-container").innerHTML += `<div class="msg-left">\
+        <div class="msg-avatar">\
+            <img src="img/persons/${localStorage.getItem("photo-script")}.jpg" alt="">\
+        </div>\
+        <div class="msg-content">\
+            <strong>${localStorage.getItem("username-script")}</strong>\
+            <p>${localStorage.getItem("messages").split("s-p-l-i-t-t-e-r")[i]}</p>\
+        </div>\
+    </div>`
+    if(saves){
+        history.push({"author": "interlocutor", "message": localStorage.getItem("messages").split("s-p-l-i-t-t-e-r")[i]})
+        save()
+    }
+    
+}
+
+function displayYourMessage(message, saves){
+    if(localStorage.getItem("messages").split("s-p-l-i-t-t-e-r")[i] == "") return
     document.getElementById("messages-container").innerHTML += `<div class="msg-right">\
         <div class="msg-avatar">\
             <img src="img/persons/2.jpg" id="your-avatar">\
         </div>\
         <div class="msg-content">\
             <strong>Вы</strong>\
-            <p>${document.getElementById("msg").value}</p>\
+            <p>${message}</p>\
         </div>\
     </div>`
+    if(saves){
+        history.push({"author": "you", "message": message})
+        save()
+    }
+}
+
+function send(){
+    if(document.getElementById("msg").value == ""){
+        displayMessage(true)
+        i++
+        return
+    }
+    displayYourMessage(document.getElementById("msg").value, true)
     loadPhoto()
 
     document.getElementById("msg").value = ""
     document.getElementById("msg").focus()
 
     if(i >= localStorage.getItem("messages").split("s-p-l-i-t-t-e-r").length) return
+    if(localStorage.getItem("messages").split("s-p-l-i-t-t-e-r")[i] == "") {
+        i++
+        return
+    }
     
     setTimeout(() => {
-        document.getElementById("messages-container").innerHTML += `<div class="msg-left">\
-            <div class="msg-avatar">\
-                <img src="img/persons/${localStorage.getItem("photo-script")}.jpg" alt="">\
-            </div>\
-            <div class="msg-content">\
-                <strong>${localStorage.getItem("username-script")}</strong>\
-                <p>${localStorage.getItem("messages").split("s-p-l-i-t-t-e-r")[i]}</p>\
-            </div>\
-        </div>`
-    
+        displayMessage()
         i++
-    }, Math.random() * 5000)
+    }, Math.random() * 8000 + 2000)
 }
 
 function loadPhoto() {
@@ -49,3 +99,8 @@ function loadPhoto() {
         }
     }
 }
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter") {
+        send();
+    }
+});
